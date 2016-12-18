369c0bba7c056f465c5d1f6b4f9a136c3f9b282c
zhaixm <zxmsdyz@sina.com>
2016-12-18 16:09:35 +0800

    pc上去掉选片精修的返回按钮

--- src/app/constant/config.ts
+++ src/app/constant/config.ts
@@ -1,13 +1,14 @@
+import {environment} from "../../environments/environment";
 function getDomain(domain: any){
   return domain.config.domain
 }
 
-export const DOMAIN = getDomain(window)
-// export const DOMAIN = environment.serverDomain
+// export const DOMAIN = getDomain(window)
+export const DOMAIN = environment.serverDomain
 
 function getQiniuDomain(domain: any){
   return domain.config.qiniuDomain
 }
 
-export const QINIU_DOMAIN = getQiniuDomain(window)
-// export const QINIU_DOMAIN = environment.qiniuDomain
+// export const QINIU_DOMAIN = getQiniuDomain(window)
+export const QINIU_DOMAIN = environment.qiniuDomain
